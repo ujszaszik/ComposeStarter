d95cd25870098f5e43a54a94a94f6afed1ccb28e
Ujszászi Károly <ujszaszi.karesz@gmail.com>
2022-01-16 20:46:40 +0100

    Small fixes on Main Activity

--- app/src/main/java/hu/ujszaszik/composestarter/ui/MainActivity.kt
+++ app/src/main/java/hu/ujszaszik/composestarter/ui/MainActivity.kt
@@ -4,25 +4,21 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
 import hu.ujszaszik.composestarter.navigation.graph.ApplicationGraph
 import hu.ujszaszik.composestarter.navigation.graph.LocalNavController
-import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    val viewModel: MainViewModel by viewModels()
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-        installSplashScreen()
         setContent {
             CompositionLocalProvider(LocalNavController provides rememberNavController()) {
                 ApplicationGraph()
